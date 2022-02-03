c538bf222dc4156b2a66590b1580e17b61b19f17
nacarney <nacarney@tcd.ie>
2022-02-03 16:13:59 +0000

    moved comment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,6 @@ import React, { useState } from 'react';
 import { Button } from "@material-ui/core";
 import ComboBox from "../components/form-items/ComboBox";
 
-
-// autocomplete and relevant State code pulled from 
-// https://stackoverflow.com/questions/59783148/programmatically-set-value-in-material-ui-autocomplete-textfield
-
 const departments = [
   'African Plains',
   'Near Side',
